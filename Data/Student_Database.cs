2fd93a78e0fbfb0970df54050abaad0a5baa7c31
Jatinder Singh <jatinder303@gmail.com>
2021-09-06 12:00:04 +1200

    Add project files.

---
+++ Data/Student_Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Student_Web_API_Xamarin_Backend.Models;
+
+namespace Student_Web_API_Xamarin_Backend.Data
+{
+    public class Student_Database : DbContext
+    {
+        public Student_Database (DbContextOptions<Student_Database> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student_Web_API_Xamarin_Backend.Models.Student> Student { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Web_API_Xamarin_Backend.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string email { get; set; }
+    }
+}
